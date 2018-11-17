ebcd6cb1a77f85184938ac4d5388b94c05ea2a07
prazd <alexokda@list.ru>
2018-11-17 03:08:07 +0300

    test balance func

---
+++ client-operator/ether/listener/test/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"../abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+var balance string
+
+func main() {
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	var balanceChannel chan string = make(chan string)
+
+	go Balance(balanceChannel)
+	go UpdateBalance(balanceChannel, &balance)
+
+	wg.Wait()
+}
+
+func Balance(c chan string) {
+	for {
+		ethClient, err := ethclient.Dial("https://rinkeby.infura.io")
+		if err != nil {
+			log.Println(err)
+		}
+		instance, err := abi.NewToken(common.HexToAddress("0xe50466567878fca56c55ee3c5f988d3ce88b5958"), ethClient) // TLCP Token
+		if err != nil {
+			log.Println(err)
+		}
+		balance, err := instance.BalanceOf(nil, common.HexToAddress("0xa0560e36f9d48dd82d8ed8c94a5d6b56020ed43b"))
+		if err != nil {
+			log.Println(err)
+		}
+		c <- balance.String()
+	}
+}
+
+func UpdateBalance(c chan string, balance *string) {
+	for {
+		msg := <-c
+		*balance = msg
+		fmt.Println(*balance)
+		time.Sleep(time.Second * 2)
+	}
+}
